bebe2cbc9572592fdfb43e0c789ae0e58a868589
Soumyajit Das <153814779+soumyajitxedu@users.noreply.github.com>
2025-06-21 21:39:48 +0530

    day1-only-print.java
    day 1 , it give output 3 integers value and sum of them

---
+++ day1-only-print.java
@@ -0,0 +1,11 @@
+public class day1
+  {
+public static void main(String[] args)
+    {
+      int a = 10;
+      int b = 20;
+      int c = 30;
+      int d = a + b + c ;
+      System.out.println(" intergers of a + b + C" + d);
+    }
+  }
