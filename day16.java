ebc139d1fc43b435024773891a5d3747cdc0eb91
Soumyajit Das <153814779+soumyajitxedu@users.noreply.github.com>
2025-10-11 18:42:36 +0530

    day 16

---
+++ day16.java
@@ -0,0 +1,17 @@
+//Write a Java program to declare two integer variables, side1 as 8 and side2 as 15. Calculate the area of a right-angled triangle (Area =  
+//2
+//1
+
+// ×base×height), and print the result to the console.
+class day16
+
+{
+    public static void main(String[] args) {
+        int a1 = 8;
+        int a2 = 15;
+        double area = 0.5*a1*a2;
+        System.out.println("side one is " + a1);
+        System.out.println("side two is "+ a2);
+        System.out.println("area is "+ area);
+    }
+}
