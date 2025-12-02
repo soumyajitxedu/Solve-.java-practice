e15dd3abfd57c7b02457b6c20fd801477804eca0
Soumyajit Das <153814779+soumyajitxedu@users.noreply.github.com>
2025-12-02 20:03:58 +0530

    Add files via upload

---
+++ day20.java
@@ -0,0 +1,17 @@
+
+class day20
+{
+public static void main(String[] args) {
+    int age = 17;
+    if (age >= 18) {
+        System.out.println("you son of bitch! , you can't give vote  ");
+    }
+    else if (age <= 0) {
+        System.out.println("you fucker don't tell lie to me");
+        
+    }
+    else{
+        System.out.println("motherfucker your child");
+    }
+}
+}
---
+++ day20_streak2.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class day20_streak2
+{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int age ;
+        System.out.println("enter your age ");
+        age = scanner.nextInt();
+        if (age > 10) {
+            System.out.println("your bitch");
+
+            
+        }
+        else{
+            System.out.println("hi");
+
+        }
+        
+        
+
+    }
+}
