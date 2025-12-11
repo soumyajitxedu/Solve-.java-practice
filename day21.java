e2c7e62b0398824cde9ff0ddffc10f5069ce9e7f
Soumyajit Das <153814779+soumyajitxedu@users.noreply.github.com>
2025-12-11 22:01:50 +0530

    Add files via upload

---
+++ day21.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+/* write program that takes input age of person from user as int , now check the wheater the person is eligiable or not ? */
+
+class day21{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("hi , i'm programmed to check your age and give insigth ,reaistically you came here do that maybe so");
+        int a = sc.nextInt();
+        System.out.println("processing im trying to find out , wait little bit");
+        System.out.println("i found it");
+        if(a <= 18){
+            System.out.println("your age is not suitable for this so go away bitch");
+        }
+        else{
+            System.out.println("i think your ? , sound goods , you can vote ")
+        }
+
+        
+    }
+
+}
+
