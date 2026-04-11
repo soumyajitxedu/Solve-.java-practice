b311df3c0b8b1dfb386556296cff367faea2cd9a
Soumyajit Das <153814779+soumyajitxedu@users.noreply.github.com>
2026-04-11 23:01:48 +0530

    Add files via upload

---
+++ nested_loops.java
@@ -0,0 +1,10 @@
+public class nested_loops{
+    public static void main(String[] args) {
+        for(int i = 1; i <= 3; i++)
+        {
+            for(int j = 1;j < 7; j++){
+                System.out.println("java nested loops" + i + ""+ j);
+            }
+        }
+    }
+}
