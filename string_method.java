99134a5fc18a4acad62752aa49d54b557e30a3dc
Soumyajit Das <153814779+soumyajitxedu@users.noreply.github.com>
2026-05-03 21:58:08 +0530

    Add files via upload

---
+++ string_method.java
@@ -0,0 +1,14 @@
+public class string_method
+{
+    public static void main(String[] args) {
+        String name = "soumyajit das";
+        int len = name.length();
+        char letter = name.charAt(2);
+
+
+        System.out.println(len);
+        System.out.print(letter);
+
+
+    }
+}
