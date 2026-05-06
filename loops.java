a7ddaeee448f389667730517c4db38ce0e9bd90b
Soumyajit Das <153814779+soumyajitxedu@users.noreply.github.com>
2026-05-06 22:03:54 +0530

    Add files via upload

---
+++ loops.java
@@ -0,0 +1,18 @@
+public class loops
+
+
+
+{
+    public static void main(String[] args)
+        {
+    int i ;
+    int j;
+
+    {
+        for(i = 1; i < 10; i ++){
+            for (j = 2; i <= i ; j++ ){
+                System.out.println(" ");
+            }
+        }
+    }
+}}
