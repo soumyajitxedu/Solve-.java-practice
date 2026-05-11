342412db1a30bd9e87be5c48fc7b5a33daa3facc
Soumyajit Das <153814779+soumyajitxedu@users.noreply.github.com>
2026-05-11 22:45:59 +0530

    Add files via upload

---
+++ consturter.java
@@ -0,0 +1,20 @@
+class consturter
+{
+
+    int bill;
+
+    int total;
+    double avarge;
+
+
+    void main()
+    {
+        bill = 1000;
+        total = bill + 500;
+        avarge = total / 2;
+
+        System.out.println("the total bill is : " + total);
+        System.out.println("the avarge bill is : " + avarge);
+        
+    }
+}
