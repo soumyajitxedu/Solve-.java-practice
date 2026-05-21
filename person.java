3a507ac31125862e6b1f3faddc78410482604c81
Soumyajit Das <153814779+soumyajitxedu@users.noreply.github.com>
2026-05-21 22:48:15 +0530

    Add files via upload

---
+++ person.java
@@ -0,0 +1,15 @@
+class person {
+    void add()
+    {
+        int a = 10;
+        int b = 1093;
+        int c = a + b;
+        System.out.println(c);
+
+
+    }
+    public static void main(String[] args){
+        person p1 = new person();
+        p1.add();
+    }
+}
